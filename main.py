67c7b6b65586de500aafc15cc88a50c740f7e080
Mozammal Haque Tonoy <realtonoy@gmail.com>
2026-04-11 01:28:25 +0600

    Lecture 3 completed

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open('patients.json') as f:
+        data = json.load(f)
+    return data
+
 @app.get("/")
 def hello():
-    return {'message' : 'Hello world'}
+    return {'message' : 'Patient Management System API'}
 
 @app.get("/about")
 def hello():
-    return {'message' : 'XYZ is an educational platform'}
+    return {'message' : 'A fully functional API to manage your patient records'}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
